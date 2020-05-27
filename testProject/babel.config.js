459084edb12325e0a974b7b89bb7488cdbce34ea
James Taber <jmtaber129@gmail.com>
2020-05-27 15:05:39 -0500

    Add test app

---
+++ testProject/babel.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: ['../instrumentor.js'],
+  // plugins: [ "./node_modules/@heap/react-native-heap/instrumentor/src/index.js"]
+};
